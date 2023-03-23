d7c397cec41bbe3505423d5a96f5214c45376458
PimPTP <princesspim.ptp@gmail.com>
2023-03-23 16:35:05 +0700

    v6

--- l300/Core/Src/main.c
+++ l300/Core/Src/main.c
@@ -51,7 +51,8 @@ UART_HandleTypeDef huart2;
 /* USER CODE BEGIN PV */
 uint32_t InputCaptureBuffer[IC_BUFFER_SIZE];
 float averageRisingedgePeriod;
-uint32_t MotorSetDuty = 1000;
+uint32_t MotorSetDuty = 100;
+uint32_t duty = 0;
 uint32_t MotorReadRPM;
 /* USER CODE END PV */
 
@@ -129,7 +130,8 @@ int main(void)
 		  timestamp = HAL_GetTick() + 500;
 		  averageRisingedgePeriod = IC_Calc_Period();
 
-		  __HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,MotorSetDuty);
+		  duty = MotorSetDuty*25;
+		  __HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,duty);
 
 	  }
   }
@@ -205,7 +207,7 @@ static void MX_TIM1_Init(void)
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 83;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 1000;
+  htim1.Init.Period = 2500;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -229,7 +231,7 @@ static void MX_TIM1_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 1000;
+  sConfigOC.Pulse = 500;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
